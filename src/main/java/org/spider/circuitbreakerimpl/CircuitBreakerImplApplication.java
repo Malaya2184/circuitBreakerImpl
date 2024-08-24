0e4a9ddad3b1e7453907d3cd6795c51ee547d8f5
Malaya2184 <malayakumarswain8@gmail.com>
2024-08-24 13:18:05 +0530

    circuit breaker project init

---
+++ src/main/java/org/spider/circuitbreakerimpl/CircuitBreakerImplApplication.java
@@ -0,0 +1,13 @@
+package org.spider.circuitbreakerimpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitBreakerImplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CircuitBreakerImplApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/spider/circuitbreakerimpl/CircuitBreakerImplApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spider.circuitbreakerimpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitBreakerImplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
